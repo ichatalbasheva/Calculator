a33ede78780693fc0fdee952bb2517588d7b7ea7
ichatalbasheva <45803509+ichatalbasheva@users.noreply.github.com>
2019-01-03 14:36:00 -0800

    refactoring vars

--- src/com/example/java/Calculator.java
+++ src/com/example/java/Calculator.java
@@ -7,7 +7,7 @@ import static com.example.java.util.MathMethods.*;
 public class Calculator {
 
     public static void main(String[] args) {
-        String s1 = getInput("Enter a numeric value: ");
+        String string1 = getInput("Enter a numeric value: ");
         String s2 = getInput("Enter a numeric value: ");
         String op = getInput("Choose an operation (+ - * /):");
 
@@ -16,16 +16,16 @@ public class Calculator {
         try {
             switch (op) {
                 case "+":
-                    result = addValues(s1, s2);
+                    result = addValues(string1, s2);
                     break;
                 case "-":
-                    result = subtractValues(s1, s2);
+                    result = subtractValues(string1, s2);
                     break;
                 case "*":
-                    result = multiplyValues(s1, s2);
+                    result = multiplyValues(string1, s2);
                     break;
                 case "/":
-                    result = divideValues(s1, s2);
+                    result = divideValues(string1, s2);
                     break;
                 default:
                     System.out.println("Unrecognized operation!");
